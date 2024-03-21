6335e2841c325c47d7e6777e65a26806f8961436
Xalovald <guilian.bahno@laposte.net>
2024-03-21 11:12:30 +0100

    import du projet

---
+++ TalentZ/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TalentZ
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
